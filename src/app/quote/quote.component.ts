dee0e565173514373519e6044e6c13204e75ad5d
Ephraim-Kamau <kephraim19@gmail.com>
2019-03-22 11:52:54 +0300

    Add the quote descriptions

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,14 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
-quotes = [
-      new Quote(1, 'Today is going to be a wonderful day'),
-      new Quote(2, 'I am going to be a great developer'),
-      new Quote(3, 'I am El Jefe!!'),
-      new Quote(4, 'Selector take your time, take your time'),
-      new Quote(5, 'Moringa School'),
-    new Quote(6, 'I know you want my free time'),
-]
+  quotes = [
+     new Quote(1, 'Today is going to be a wonderful day', 'This is because I am going to make a good project'),
+     new Quote(2, 'I am going to be a great developer', 'And get employed in a good company'),
+     new Quote(3, 'I am El Jefe!!', 'The Boss!'),
+     new Quote(4, 'Selector take your time, take your time','You are going too fast!!'),
+     new Quote(5, 'Moringa School', 'The place to be for all aspiring developers'),
+     new Quote(6, 'I know you want my free time','Frech Montana, TY $ Sign, Tinashe'),
+   ]
   constructor() { }
 
   ngOnInit() {
