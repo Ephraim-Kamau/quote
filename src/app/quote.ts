0c2501d58a78f03ae4f0d1b0e7115acf84d798ba
Ephraim-Kamau <kephraim19@gmail.com>
2019-03-22 17:44:00 +0300

    Add changes to the indentation

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
   public showDescription:boolean;
   //completeDate is timePassed
-  constructor(public id:number, public name:string, public description:string, public timePassed:Date){
-    this.showDescription=false;
+    constructor(public id:number, public name:string, public description:string, public timePassed:Date){
+      this.showDescription=false;
+    }
   }
-}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
        new Quote(1, 'Today is going to be a wonderful day', new Date(2019,3,22)),
+            ]
 
-     ]
      toogleDetails(index){
-       this.quotes[index].showDescription = !this.quotes[index].showDescription
-     }
+        this.quotes[index].showDescription = !this.quotes[index].showDescription
+        }
      addNewQuote(quote){
             let quoteLength = this.quotes.length;
             quote.id=quoteLength+1;
@@ -25,9 +25,9 @@ export class QuoteComponent implements OnInit {
 
      deleteQuote(isComplete, index){
        if (isComplete){
-         let toDelete=confirm(`Are you sure you want to delete the quote:  ${this.quotes[index].name} ?`)
+          let toDelete=confirm(`Are you sure you want to delete the quote:  ${this.quotes[index].name} ?`)
 
-         if(toDelete){
+      if(toDelete){
          this.quotes.splice(index, 1);
        }
      }
